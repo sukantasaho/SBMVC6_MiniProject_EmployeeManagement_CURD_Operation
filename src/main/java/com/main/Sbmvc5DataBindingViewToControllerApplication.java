66f8c2ac76f4c37d777ddabdfecf2aef76ffcd49
sukantasaho <ssukanta520@gmail.com>
2024-04-23 17:06:46 +0530

    commit

---
+++ src/main/java/com/main/Sbmvc5DataBindingViewToControllerApplication.java
@@ -0,0 +1,13 @@
+package com.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbmvc5DataBindingViewToControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sbmvc5DataBindingViewToControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/main/Sbmvc5DataBindingViewToControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbmvc5DataBindingViewToControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
